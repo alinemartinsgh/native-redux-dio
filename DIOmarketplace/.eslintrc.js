87915acf264eea9a3b15c6d4655b816ee84f683b
aline martins <aline.p.martins@globalhitss.com.br>
2021-08-25 10:55:46 -0300

    config eslint + prettier

--- DIOmarketplace/.eslintrc.js
+++ DIOmarketplace/.eslintrc.js
@@ -1,4 +1,37 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+    'react-hooks',
+    'jsx-a11y',
+    'import'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js']
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'no-params-reassign': 'off'
+  },
 };
